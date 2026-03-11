4848d7a2c5c6172b058c9cf20180d02dc71d10e2
priyansh3010 <pri.obhan30@gmail.com>
2026-03-11 19:30:19 +0530

    Fixed load fen function, and added extra functions to legal movegen.

--- board.h
+++ board.h
@@ -1,7 +1,9 @@
 #pragma once
+#include <string>
 #include "types.h"
 #include "move.h"
 #include "moveinfo.h"
+using namespace std;
 
 struct Board {
     U64 pieces[2][6]; // one for all different types of pieces for black and white
@@ -13,10 +15,12 @@ struct Board {
     int halfMoveClock;
     int fullMoveNumber;
 
-    void init();
+    void loadFEN(string FEN);
+    void init(string FEN = "rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1");
     void printBoard();
     Piece getPieceAt(Color color, int square) const;
     MoveInfo makeMove(Move move);
     void unMakeMove(MoveInfo moveInfo);
     bool isKingInCheck();
+    bool isSquareAttacked(int toSquare);
 };
