c2c65ac39cd05a9fae3a544e51230190cde4c938
priyansh3010 <pri.obhan30@gmail.com>
2026-03-25 19:23:31 +0530

    Updated default constructor and added == operator to struct.

--- move.h
+++ move.h
@@ -10,11 +10,16 @@ struct Move {
     bool isCastle;
     bool isEnPassant;
 
-    Move() = default;
+    Move() : fromSquare(-1), toSquare(-1), pieceType(NONE), capturedPiece(NONE), 
+             promotionPiece(NONE), isCastle(false), isEnPassant(false) {}
 
     Move(int from, int to, Piece piece, Piece captured = NONE, 
          Piece promotion = NONE, bool castle = false, bool enPassant = false)
         : fromSquare(from), toSquare(to), pieceType(piece),
           capturedPiece(captured), promotionPiece(promotion),
           isCastle(castle), isEnPassant(enPassant) {}
+    
+    bool operator==(const Move& other) const {
+        return fromSquare == other.fromSquare && toSquare == other.toSquare && promotionPiece == other.promotionPiece;
+    }
 };
